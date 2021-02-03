783fbe732b2a98fa4dd7db7115988e633b75577c
wild-e <wild.emmanuelle@gmail.com>
2021-02-03 10:59:58 +0100

    made Project entity + Message entity

---
+++ src/Entity/Project.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProjectRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProjectRepository::class)
+ */
+class Project
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="date")
+     */
+    private $addDate;
+
+    /**
+     * @ORM\Column(type="string", length=120)
+     */
+    private $title;
+
+    /**
+     * @ORM\Column(type="text")
+     */
+    private $text;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getAddDate(): ?\DateTimeInterface
+    {
+        return $this->addDate;
+    }
+
+    public function setAddDate(\DateTimeInterface $addDate): self
+    {
+        $this->addDate = $addDate;
+
+        return $this;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getText(): ?string
+    {
+        return $this->text;
+    }
+
+    public function setText(string $text): self
+    {
+        $this->text = $text;
+
+        return $this;
+    }
+}
