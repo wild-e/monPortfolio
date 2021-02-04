48fc82e6a91f11d1935464f4002d2125f5c84e55
wild-e <wild.emmanuelle@gmail.com>
2021-02-04 16:43:54 +0100

    Having fun with front stuff...

--- assets/app.js
+++ assets/app.js
@@ -8,5 +8,5 @@
 // any CSS you import will output into a single css file (app.css in this case)
 import './styles/app.css';
 
-// start the Stimulus application
-import './bootstrap';
+// JS
+import './title.js';
