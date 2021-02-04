7b351f701aadd37c341fc47f968e7ce7cf49ccfb
wild-e <wild.emmanuelle@gmail.com>
2021-02-04 14:23:43 +0100

    Having fun with twig and finished forms integrations

--- src/Form/ProjectType.php
+++ src/Form/ProjectType.php
@@ -6,15 +6,20 @@ use App\Entity\Project;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class ProjectType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('addDate')
-            ->add('title')
-            ->add('text')
+            ->add('addDate', DateType::class, [
+                'widget' => 'single_text',
+            ])
+            ->add('title', TextType::class)
+            ->add('text', TextareaType::class)
         ;
     }
 
