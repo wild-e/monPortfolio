6b5b6030b2bd19e0dc86294604f2a854510083b5
wild-e <62098185+wild-e@users.noreply.github.com>
2021-02-12 19:59:40 +0100

    Update ReCaptchaApi.php

--- src/Service/ReCaptchaApi.php
+++ src/Service/ReCaptchaApi.php
@@ -11,17 +11,16 @@ class ReCaptchaApi
         $client = HttpClient::create();
         $response = $client->request(
             'POST',
-            'https://www.google.com/recaptcha/api/siteverify?secret=6LdNyUgaAAAAAKuL5c7Iejq7Q1Tcwa86bPI2z-A6&response=' . $userResponse
+            'https://www.google.com/recaptcha/api/siteverify?secret=XXX&response=' . $userResponse
         );
         
         $statusCode = $response->getStatusCode(); // get Response status code 200
 
         if ($statusCode === 200) {
             $apiResponse = $response->getContent();
-            // get the response in JSON format
 
             $apiResponse = $response->toArray();
-            // convert the response (here in JSON) to an PHP array
+            // convert the response (here in JSON) to a PHP array
             
             return $apiResponse;
         }
