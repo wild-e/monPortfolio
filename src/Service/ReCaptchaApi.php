b0913ac3f36ca761b7fda49a545deb55f84ab6c9
wild-e <wild.emmanuelle@gmail.com>
2021-02-12 20:18:59 +0100

    Modified recaptcha api

--- src/Service/ReCaptchaApi.php
+++ src/Service/ReCaptchaApi.php
@@ -11,15 +11,13 @@ class ReCaptchaApi
         $client = HttpClient::create();
         $response = $client->request(
             'POST',
-            'https://www.google.com/recaptcha/api/siteverify?secret=6LdNyUgaAAAAAKuL5c7Iejq7Q1Tcwa86bPI2z-A6&response=' . $userResponse
+            'https://www.google.com/recaptcha/api/siteverify?secret=XXX&response=' . $userResponse
         );
         
-        $statusCode = $response->getStatusCode(); // get Response status code 200
+        $statusCode = $response->getStatusCode();
 
         if ($statusCode === 200) {
             $apiResponse = $response->getContent();
-            // get the response in JSON format
-
             $apiResponse = $response->toArray();
             // convert the response (here in JSON) to an PHP array
             
